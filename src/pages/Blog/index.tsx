f2a899bef4a16056d552945dfad712925be41da1
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-02-27 12:51:46 -0300

    feat: add search form

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../../components/Header";
 import { Profile } from "../../components/Profile";
 
-import { Container } from "./styles";
+import { Container, TitleGroup } from "./styles";
 
 export function Blog() {
   return (
@@ -10,6 +10,17 @@ export function Blog() {
 
       <Container>
         <Profile />
+
+        <main>
+          <TitleGroup>
+            <h2>Publicações</h2>
+            <span>6 publicações</span>
+          </TitleGroup>
+
+          <form>
+            <input type="text" name="search" placeholder="Buscar conteúdo" />
+          </form>
+        </main>
       </Container>
     </div>
   )
--- src/pages/Blog/styles.ts
+++ src/pages/Blog/styles.ts
@@ -6,4 +6,46 @@ export const Container = styled.div`
 
   padding: 0 1rem;
   margin: 0 auto;
-`
+
+  main {
+    margin-top: 4.5rem;
+
+    form {
+      input {
+        width: 100%;
+        height: 50px;
+
+        border-radius: 6px;
+        padding: 0.75rem 1rem;
+        border: 1px solid ${props => props.theme["base-border"]};
+
+        background-color: ${props => props.theme["base-input"]};
+        color: ${props => props.theme["base-text"]};
+        
+        &::placeholder {
+          color: ${props => props.theme["base-label"]};
+        }
+      }
+    }
+  }
+`;
+
+export const TitleGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-bottom: 0.875rem;
+
+  h2 {
+    color: ${props => props.theme["base-subtitle"]};
+
+    font-size: 1.125rem;
+  }
+
+  span {
+    color: ${props => props.theme["base-span"]};
+
+    font-size: 0.875rem;
+  }
+`;
