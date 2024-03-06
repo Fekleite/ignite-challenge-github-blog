f26f1c25355ae9a2f00120f1774d8707eaaaffa9
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-03-05 21:28:04 -0300

    feat: add user infos

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,37 +1,62 @@
+import { useEffect, useState } from 'react'
 import { FaGithub, FaBuilding, FaUserGroup } from 'react-icons/fa6'
 
 import { ExternalLink } from '../ExternalLink'
 
 import { ProfileContainer, ProfileInfo, ProfileResume } from './styles'
+import { api } from '../../services/axios';
+
+interface UserState {
+  id: number,
+  avatar_url: string;
+  name: string;
+  company: string,
+  email: string,
+  bio: string,
+  followers: number
+  html_url: string
+}
 
 export function Profile() {
+  const [user, setUser] = useState<UserState>({} as UserState);
+
+  useEffect(() => {
+    async function getUser() {
+      const { data } = await api.get<UserState>(`/users/Fekleite`)
+
+      setUser(data);
+    }
+
+    getUser()
+  }, [])
+
   return (
     <ProfileContainer>
-      <img src="" alt="" />
+      <img src={user.avatar_url} alt={user.name} />
 
       <ProfileInfo>
         <header>
-          <span>Cameron Williamson</span>
+          <span>{user.name}</span>
 
-          <ExternalLink url="https://github.com" text="github" />
+          <ExternalLink url={user.html_url} text="github" />
         </header>
 
-        <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+        <p>{user.bio}</p>
 
         <ProfileResume>
           <div>
             <FaGithub />
-            <span>cameronwll</span>
+            <span>Fekleite</span>
           </div>
 
           <div>
             <FaBuilding />
-            <span>Rocketseat</span>
+            <span>{user.company}</span>
           </div>
 
           <div>
             <FaUserGroup />
-            <span>32 seguidores</span>
+            <span>{user.followers} seguidores</span>
           </div>
         </ProfileResume>
       </ProfileInfo>
